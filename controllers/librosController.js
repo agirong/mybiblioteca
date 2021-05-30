effada4f41483233c38722de2a395ae44cdefda6
agirong <62667714+agirong@users.noreply.github.com>
2021-05-30 17:31:48 -0600

    Desing Form Interface
    Aqui modifique el formulario

--- controllers/librosController.js
+++ controllers/librosController.js
@@ -4,9 +4,12 @@ var libro= require("../model/libro");
 module.exports={
     
     index:function(req,res){
-        libro.obtener(conexion,function(err,datos){
+        libro.obtener(conexion,function (err,datos){
             console.log(datos);
-            res.render('./libros/index', { title: 'App' });
-        })
+            res.render('./libros/index', { title: 'App', libros:datos });
+        });
+    },
+    crear:function(req,res){
+        res.render('./libros/crear');
     }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
-const librosController = require("../controllers/librosController");
+
 
 /* GET home page. */
-router.get('/',librosController.index);
+router.get('/',function(req,res,next){
+    res.send("Bienvendio a la bibilioteca");
+});
 
 module.exports = router;
